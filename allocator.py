c97fd35c62faad2c5b6f08257b140cdc2f0284e5
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-04-17 19:37:03 +0200

    Added allocation algorithms

--- allocator.py
+++ allocator.py
@@ -12,6 +12,43 @@ def initialize_memory(size):
   memory = [{'start': 0, 'size': size, 'process': None}]
 
 
+# allocation algorthms implementations
+def find_best_fit(size):
+  best_fit_index = -1
+  min_diff = float('inf')
+ 
+  for i, block in enumerate(memory):
+    if block['process'] is None and block['size'] >= size:
+      diff = block['size'] - size
+      if diff < min_diff:
+        min_diff = diff
+        best_fit_index = i
+ 
+  return best_fit_index
+ 
+ 
+ 
+def find_first_fit(size):
+  for i, block in enumerate(memory):
+    if block['process'] is None and block['size'] >= size:
+      return i
+  return -1
+ 
+ 
+ 
+def find_worst_fit(size):
+  worst_fit_index = -1
+  max_diff = float('-inf')
+ 
+  for i, block in enumerate(memory):
+    if block['process'] is None and block['size'] >= size:
+      diff = block['size'] - size
+      if diff > max_diff:
+        max_diff = diff
+        worst_fit_index = i
+ 
+  return worst_fit_index
+
 
 if __name__ == "__main__":
   import sys
